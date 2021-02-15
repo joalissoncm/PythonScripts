84bdab7d12e962f4a52b3a7521af67462287b05b
Joalisson Moreira <63914002+JoalissonCM@users.noreply.github.com>
2021-02-15 11:30:15 -0300

    Update ex013.py

--- ex013.py
+++ ex013.py
@@ -1,12 +1,11 @@
 #Classic! Calcular a média do aluno com base em suas notas
-
 nota1 = float(input('Informe a primeira nota: '))
 nota2 = float(input('Informe a segunda nota: '))
 media = (nota1 + nota2) /2
 
 if media < 5:
-    print('Média {}, Aluno(a) reprovado(a)!'.format(media))
+    print(f'Média {media}, Aluno(a) reprovado(a)!')
 elif media > 5 and media < 7:
-    print('Média {}, Aluno(a) está de recuperação!'.format(media))
+    print(f'Média {media}, Aluno(a) está de recuperação!')
 elif media > 7:
-    print('Média {}, Aluno(a) Aprovado(a)!'.format(media))
+    print('Média {media}, Aluno(a) Aprovado(a)!')
