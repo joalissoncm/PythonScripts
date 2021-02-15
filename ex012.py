c94821ec5f89e6053aff317edd75254e0768f5ef
Joalisson Moreira <63914002+JoalissonCM@users.noreply.github.com>
2021-02-15 11:36:09 -0300

    Update ex012.py

--- ex012.py
+++ ex012.py
@@ -1,7 +1,6 @@
 # Programa recebe dois números inteiros e mostra qual é o maior ou se são valores iguais
 num1 = int(input('Digite o primeiro número: '))
 num2 = int(input('Digite o segundo número: '))
-
 if num1 > num2:
     print(f'O primeiro número é maior, {num1}!')
 elif num2 > num1:
