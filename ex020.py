cb5471b47d7a0c66e29cdd9f75a050566ad8a64f
Joalisson Moreira <63914002+JoalissonCM@users.noreply.github.com>
2021-02-15 11:38:59 -0300

    Update ex020.py

--- ex020.py
+++ ex020.py
@@ -2,7 +2,6 @@
 valores = list()
 for cont in range(0, 5):    #For para guardar os númeors na lista
     valores.append(int(input('Digite um número: ')))
-
 for c, v in enumerate(valores):
     print(f'Na posição {c} encontrei o valor {v}!')
 print('Cheguei ao final da lista.')
