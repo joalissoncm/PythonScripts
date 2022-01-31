5ce18fb8f1c1563ce415f6ea948dbf099466de52
Joalisson da Costa Moreira <63914002+joalissoncm@users.noreply.github.com>
2022-01-31 18:08:02 -0300

    Delete ex029.py

--- scripts/ex029.py
+++
@@ -1,8 +0,0 @@
-def area(a, b):
-    area = largura * comprimento
-    print(f'A área de um terreno {largura}x{comprimento} é de {area}m²')
-
-
-largura = float(input('LARGURA: (m): '))
-comprimento = float(input('COMPRIMENTO (m): '))
-area(largura, comprimento)
