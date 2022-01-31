56602787892d7ea4b0756b796e6deaa2e06be7b0
Joalisson da Costa Moreira <63914002+joalissoncm@users.noreply.github.com>
2022-01-31 17:54:30 -0300

    Delete ex006.py

--- scripts/ex006.py
+++
@@ -1,5 +0,0 @@
-# Ler duas notas de um aluno(a) e mostra a média
-n1 = int(input('Digite a primeira nota: '))
-n2 = int(input('Digite a segunda nota: '))
-media = (n1+n2)/2
-print('A média é: ', media)
