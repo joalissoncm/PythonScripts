dfa817248fd3dfb5e6a13cc6e9fc2ce943d2b8e7
Joalisson da Costa Moreira <63914002+joalissoncm@users.noreply.github.com>
2022-01-31 17:53:43 -0300

    Add files via upload

---
+++ scripts/Básico/ex009.py
@@ -0,0 +1,6 @@
+#Programa recebe o cateto oposto e o adjacente e retorma o comprimento da hipotenusa
+import math
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+hipot = math.hypot(co, ca)
+print('A hipotenusa vai medir {:.2f}'.format(hipot))
