08c345ba66e73193b27ae46ab1c814f2cfd6ff2c
Joalisson da Costa Moreira <63914002+joalissoncm@users.noreply.github.com>
2022-01-31 17:22:56 -0300

    Delete ex034.py

--- ex034.py
+++
@@ -1,9 +0,0 @@
-#Exercício do curso de Python do Kaggle
-# Your code goes here. Define a function called 'sign'
-def sign(x):
-    if x > 0:
-        return 1
-    elif x < 0:
-        return -1
-    else:
-        return 0
